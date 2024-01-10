314ba4bb3337ac0fa360b964ce194948e44b0505
Alexander Osipov <aosipv@yandex.ru>
2024-01-10 19:03:42 +0400

    del import

--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,6 @@ import { ThemeProvider } from 'styled-components';
 import App from './App.tsx';
 import { ResetStyle, theme } from './styles';
 
-import './index.css';
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
